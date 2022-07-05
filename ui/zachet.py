4dbb8616a13ca8d15a7cafdd6a3323238341dde8
alexxxesss <alexeenkoalexxx@yandex.ru>
2022-07-06 01:14:30 +0300

    zachet

--- ui/zachet.py
+++ ui/zachet.py
@@ -15,17 +15,24 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QLineEdit,
-    QPlainTextEdit, QPushButton, QSizePolicy, QTabWidget,
-    QTableView, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+    QGridLayout, QHBoxLayout, QHeaderView, QLabel,
+    QLineEdit, QPlainTextEdit, QPushButton, QSizePolicy,
+    QTabWidget, QTableView, QTextEdit, QVBoxLayout,
+    QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(680, 588)
+        Form.resize(606, 535)
         self.gridLayout = QGridLayout(Form)
         self.gridLayout.setObjectName(u"gridLayout")
+        self.pushButtonLogin = QPushButton(Form)
+        self.pushButtonLogin.setObjectName(u"pushButtonLogin")
+
+        self.gridLayout.addWidget(self.pushButtonLogin, 0, 1, 1, 1)
+
         self.tabWidget = QTabWidget(Form)
         self.tabWidget.setObjectName(u"tabWidget")
         self.tab = QWidget()
@@ -58,24 +65,147 @@ class Ui_Form(object):
         self.tabWidget.addTab(self.tab_3, "")
         self.tab_2 = QWidget()
         self.tab_2.setObjectName(u"tab_2")
-        self.get_one_todo = QPushButton(self.tab_2)
-        self.get_one_todo.setObjectName(u"get_one_todo")
-        self.get_one_todo.setGeometry(QRect(260, 250, 75, 24))
-        self.plainTextEdit_2 = QPlainTextEdit(self.tab_2)
-        self.plainTextEdit_2.setObjectName(u"plainTextEdit_2")
-        self.plainTextEdit_2.setGeometry(QRect(160, 310, 256, 192))
-        self.tableView_2 = QTableView(self.tab_2)
-        self.tableView_2.setObjectName(u"tableView_2")
-        self.tableView_2.setGeometry(QRect(170, 10, 256, 192))
+        self.verticalLayout_2 = QVBoxLayout(self.tab_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label = QLabel(self.tab_2)
+        self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(100, 22))
+        self.label.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.label.setLayoutDirection(Qt.LeftToRight)
+        self.label.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout.addWidget(self.label)
+
         self.lineEdit = QLineEdit(self.tab_2)
         self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setGeometry(QRect(230, 210, 113, 22))
+        self.lineEdit.setMinimumSize(QSize(120, 22))
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_2 = QLabel(self.tab_2)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setMinimumSize(QSize(100, 22))
+        self.label_2.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.label_2.setLayoutDirection(Qt.LeftToRight)
+        self.label_2.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
+
+        self.horizontalLayout_2.addWidget(self.label_2)
+
+        self.textEdit = QTextEdit(self.tab_2)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setMinimumSize(QSize(120, 0))
+
+        self.horizontalLayout_2.addWidget(self.textEdit)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_3 = QLabel(self.tab_2)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMinimumSize(QSize(100, 22))
+        self.label_3.setMaximumSize(QSize(80, 16777215))
+        self.label_3.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.label_3.setLayoutDirection(Qt.LeftToRight)
+        self.label_3.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_3.addWidget(self.label_3)
+
+        self.checkBox = QCheckBox(self.tab_2)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setMinimumSize(QSize(120, 22))
+
+        self.horizontalLayout_3.addWidget(self.checkBox)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_4 = QLabel(self.tab_2)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setMinimumSize(QSize(100, 22))
+        self.label_4.setMaximumSize(QSize(80, 16777215))
+        self.label_4.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.label_4.setLayoutDirection(Qt.LeftToRight)
+        self.label_4.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_4.addWidget(self.label_4)
+
+        self.checkBox_2 = QCheckBox(self.tab_2)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+        self.checkBox_2.setMinimumSize(QSize(120, 22))
+
+        self.horizontalLayout_4.addWidget(self.checkBox_2)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_5 = QLabel(self.tab_2)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setMinimumSize(QSize(100, 22))
+        self.label_5.setMaximumSize(QSize(80, 16777215))
+        self.label_5.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.label_5.setLayoutDirection(Qt.LeftToRight)
+        self.label_5.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_5.addWidget(self.label_5)
+
+        self.comboBox = QComboBox(self.tab_2)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setMinimumSize(QSize(120, 22))
+
+        self.horizontalLayout_5.addWidget(self.comboBox)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_5)
+
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_6 = QLabel(self.tab_2)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setMinimumSize(QSize(100, 22))
+        self.label_6.setMaximumSize(QSize(80, 16777215))
+        self.label_6.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.label_6.setLayoutDirection(Qt.LeftToRight)
+        self.label_6.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_6.addWidget(self.label_6)
+
+        self.dateTimeEdit = QDateTimeEdit(self.tab_2)
+        self.dateTimeEdit.setObjectName(u"dateTimeEdit")
+        self.dateTimeEdit.setMinimumSize(QSize(120, 22))
+        self.dateTimeEdit.setContextMenuPolicy(Qt.DefaultContextMenu)
+        self.dateTimeEdit.setCurrentSection(QDateTimeEdit.YearSection)
+        self.dateTimeEdit.setCalendarPopup(True)
+        self.dateTimeEdit.setTimeSpec(Qt.UTC)
+
+        self.horizontalLayout_6.addWidget(self.dateTimeEdit)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_6)
+
+        self.create_todo = QPushButton(self.tab_2)
+        self.create_todo.setObjectName(u"create_todo")
+
+        self.verticalLayout_2.addWidget(self.create_todo)
+
         self.tabWidget.addTab(self.tab_2, "")
         self.tab_4 = QWidget()
         self.tab_4.setObjectName(u"tab_4")
         self.tabWidget.addTab(self.tab_4, "")
 
-        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.tabWidget, 1, 1, 1, 1)
 
 
         self.retranslateUi(Form)
@@ -88,10 +218,19 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButtonLogin.setText(QCoreApplication.translate("Form", u"\u0410\u0432\u0442\u043e\u0440\u0438\u0437\u043e\u0432\u0430\u0442\u044c\u0441\u044f", None))
         self.get_all_todo.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u0441\u043f\u0438\u0441\u043e\u043a \u0432\u0441\u0435\u0445 \u0434\u0435\u043b", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Form", u"\u0421\u043f\u0438\u0441\u043e\u043a \u0432\u0441\u0435\u0445 \u0434\u0435\u043b", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("Form", u"\u041a\u043e\u043c\u043c\u0435\u043d\u0442\u0430\u0440\u0438\u0438", None))
-        self.get_one_todo.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.label.setText(QCoreApplication.translate("Form", u"\u0417\u0430\u0433\u043e\u043b\u043e\u0432\u043e\u043a", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"\u0422\u0435\u043a\u0441\u0442 \u0437\u0430\u0434\u0430\u043d\u0438\u044f", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"\u041f\u0443\u0431\u043b\u0438\u0447\u043d\u0430\u044f", None))
+        self.checkBox.setText("")
+        self.label_4.setText(QCoreApplication.translate("Form", u"\u0412\u0430\u0436\u043d\u0430\u044f", None))
+        self.checkBox_2.setText("")
+        self.label_5.setText(QCoreApplication.translate("Form", u"\u0421\u0442\u0430\u0442\u0443\u0441", None))
+        self.label_6.setText(QCoreApplication.translate("Form", u"\u0421\u0440\u043e\u043a \u0432\u044b\u043f\u043e\u043b\u043d\u0435\u043d\u0438\u044f", None))
+        self.create_todo.setText(QCoreApplication.translate("Form", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c \u0437\u0430\u043f\u0438\u0441\u044c", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Form", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c \u0434\u0435\u043b\u043e", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("Form", u"\u041e \u043f\u0440\u043e\u0433\u0440\u0430\u043c\u043c\u0435", None))
     # retranslateUi
--- zachet_alexeenko.py
+++ zachet_alexeenko.py
@@ -3,6 +3,7 @@ import requests
 from PySide6 import QtWidgets, QtCore, QtGui
 
 from ui.zachet import Ui_Form
+from ui.login import Ui_FormLogin
 
 
 class DjangoWebQt(QtWidgets.QWidget):
@@ -17,8 +18,15 @@ class DjangoWebQt(QtWidgets.QWidget):
 
         self.initUi()
 
+        self.child_window = Login()
+
     def initUi(self):
+        self.setWindowTitle("Работа с БД через API")
+        self.setFixedSize(965, 600)
         self.ui.get_all_todo.clicked.connect(self.getAllToDo)
+        self.ui.pushButtonLogin.clicked.connect(self.onPushButtonLogin)
+        self.ui.comboBox.addItems(['Активно', 'Отложено', 'Выполнено'])
+        self.ui.create_todo.clicked.connect(self.postToDo)
 
     def getAllToDo(self):
         resp = requests.get(f"{self.url}/api/v1/todo/")
@@ -50,6 +58,44 @@ class DjangoWebQt(QtWidgets.QWidget):
 
         self.ui.tableView.setModel(stm)
 
+    def postToDo(self):
+
+        title = self.ui.lineEdit.text()
+        message = self.ui.textEdit.toPlainText()
+
+        if self.ui.checkBox.isChecked():
+            public = True
+        else:
+            public = False
+
+        if self.ui.checkBox_2.isChecked():
+            importance = True
+        else:
+            importance = False
+
+        dict_status = {
+            "Активно": "Active",
+            "Отложено": "Delayed",
+            "Выполнено": "Finish"
+        }
+
+        status = dict_status[self.ui.comboBox.currentText()]
+
+        deadline = self.ui.dateTimeEdit.dateTime().toString("yyyy-MM-ddTHH:mm:ss")
+
+        dict_todo = dict(
+            title=title,
+            message=message,
+            public=public,
+            importance=importance,
+            status=status,
+            deadline=deadline
+        )
+        print(dict_todo)
+
+    def onPushButtonLogin(self):
+        self.child_window.show()
+
     def closeEvent(self, event: QtGui.QCloseEvent) -> None:
         reply = QtWidgets.QMessageBox.question(self,
                                                'Закрыть окно?', 'Вы хотите закрыть окно?',
@@ -59,6 +105,33 @@ class DjangoWebQt(QtWidgets.QWidget):
             event.accept()
         else:
             event.ignore()
+
+
+class Login(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_FormLogin()
+        self.ui.setupUi(self)
+
+        self.initUi()
+
+    def initUi(self):
+        self.setWindowTitle("Авторизация")
+        self.ui.pushButtonLogin.clicked.connect(self.onPushButtonLogin)
+
+    def onPushButtonLogin(self):
+        username = self.ui.lineEdit.text()
+        password = self.ui.lineEdit_2.text()
+        self.dict_login = dict(username=username, password=password)
+
+        request = requests.post(f"http://127.0.0.1:8000/api/v1/login/", json=self.dict_login)
+
+        if request.status_code == 200:
+            QtWidgets.QMessageBox.warning(self, "Поздравляем", "Вы вошли в систему!")
+            self.close()
+        else:
+            QtWidgets.QMessageBox.warning(self, "Внимание", "Данные введены неправильно!")
 
 
 if __name__ == "__main__":
