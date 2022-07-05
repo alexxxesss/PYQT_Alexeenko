b5b0d2643214340507c5f63e9d827952f028340c
alexxxesss <alexeenkoalexxx@yandex.ru>
2022-07-05 17:42:03 +0300

    zachet

--- ui/zachet.py
+++ ui/zachet.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QPushButton,
-    QSizePolicy, QTabWidget, QTableView, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QLineEdit,
+    QPlainTextEdit, QPushButton, QSizePolicy, QTabWidget,
+    QTableView, QVBoxLayout, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -39,17 +39,37 @@ class Ui_Form(object):
 
         self.verticalLayout.addWidget(self.tableView)
 
-        self.pushButton = QPushButton(self.tab)
-        self.pushButton.setObjectName(u"pushButton")
+        self.get_all_todo = QPushButton(self.tab)
+        self.get_all_todo.setObjectName(u"get_all_todo")
 
-        self.verticalLayout.addWidget(self.pushButton)
+        self.verticalLayout.addWidget(self.get_all_todo)
+
+        self.plainTextEdit = QPlainTextEdit(self.tab)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+
+        self.verticalLayout.addWidget(self.plainTextEdit)
 
 
         self.gridLayout_2.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
         self.tabWidget.addTab(self.tab, "")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName(u"tab_3")
+        self.tabWidget.addTab(self.tab_3, "")
         self.tab_2 = QWidget()
         self.tab_2.setObjectName(u"tab_2")
+        self.get_one_todo = QPushButton(self.tab_2)
+        self.get_one_todo.setObjectName(u"get_one_todo")
+        self.get_one_todo.setGeometry(QRect(260, 250, 75, 24))
+        self.plainTextEdit_2 = QPlainTextEdit(self.tab_2)
+        self.plainTextEdit_2.setObjectName(u"plainTextEdit_2")
+        self.plainTextEdit_2.setGeometry(QRect(160, 310, 256, 192))
+        self.tableView_2 = QTableView(self.tab_2)
+        self.tableView_2.setObjectName(u"tableView_2")
+        self.tableView_2.setGeometry(QRect(170, 10, 256, 192))
+        self.lineEdit = QLineEdit(self.tab_2)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(230, 210, 113, 22))
         self.tabWidget.addTab(self.tab_2, "")
         self.tab_4 = QWidget()
         self.tab_4.setObjectName(u"tab_4")
@@ -60,7 +80,7 @@ class Ui_Form(object):
 
         self.retranslateUi(Form)
 
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(2)
 
 
         QMetaObject.connectSlotsByName(Form)
@@ -68,8 +88,10 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.pushButton.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u0441\u043f\u0438\u0441\u043e\u043a \u0432\u0441\u0435\u0445 \u0434\u0435\u043b", None))
+        self.get_all_todo.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u0441\u043f\u0438\u0441\u043e\u043a \u0432\u0441\u0435\u0445 \u0434\u0435\u043b", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Form", u"\u0421\u043f\u0438\u0441\u043e\u043a \u0432\u0441\u0435\u0445 \u0434\u0435\u043b", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("Form", u"\u041a\u043e\u043c\u043c\u0435\u043d\u0442\u0430\u0440\u0438\u0438", None))
+        self.get_one_todo.setText(QCoreApplication.translate("Form", u"PushButton", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Form", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c \u0434\u0435\u043b\u043e", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("Form", u"\u041e \u043f\u0440\u043e\u0433\u0440\u0430\u043c\u043c\u0435", None))
     # retranslateUi
--- zachet_alexeenko.py
+++ zachet_alexeenko.py
@@ -1,3 +1,5 @@
+import requests
+
 from PySide6 import QtWidgets, QtCore, QtGui
 
 from ui.zachet import Ui_Form
@@ -11,10 +13,52 @@ class DjangoWebQt(QtWidgets.QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
+        self.url = 'http://127.0.0.1:8000'
+
         self.initUi()
 
     def initUi(self):
-        ...
+        self.ui.get_all_todo.clicked.connect(self.getAllToDo)
+
+    def getAllToDo(self):
+        resp = requests.get(f"{self.url}/api/v1/todo/")
+        list_todo = resp.json()
+        new_list_todo = []
+        self.ui.plainTextEdit.setPlainText(str(list_todo))
+
+        headers = [
+            "Автор",
+            "Название",
+            "Текст TODO",
+            "Публичная",
+            "Важная",
+            "Статус",
+            "Крайний срок",
+            "Дата создания",
+            "Дата обновления"
+        ]
+        stm = QtGui.QStandardItemModel()
+        stm.setHorizontalHeaderLabels(headers)
+
+        key_dict = []
+        for key in list_todo[0].keys():
+            key_dict.append(key)
+
+        for row in range(len(list_todo)):
+            for i in range(len(headers)):
+                stm.setItem(row, i, QtGui.QStandardItem(str(list_todo[row][key_dict[i]])))
+
+        self.ui.tableView.setModel(stm)
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        reply = QtWidgets.QMessageBox.question(self,
+                                               'Закрыть окно?', 'Вы хотите закрыть окно?',
+                                               QtWidgets.QMessageBox.Yes,
+                                               QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == "__main__":
